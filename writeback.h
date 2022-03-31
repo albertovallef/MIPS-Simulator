103fe91074a4241e6459e917f98f649e02faaa43
Alberto <albertovalleflores99@gmail.com>
2022-03-31 15:32:16 -0700

    Add Part1 complete checkpoint

--- writeback.h
+++ writeback.h
@@ -5,7 +5,7 @@ using namespace std;
 
 int total_clock_cycles = 0;
 
-void writeback(string destReg, int value, bool memtoReg, bool memWrite){
+void writeback(string destReg, int alu_result, bool memtoReg, bool memWrite){
     total_clock_cycles++;
     cout << "total_clock_cycles " + to_string(total_clock_cycles) << endl;
     if(destReg == "none") {
@@ -13,16 +13,16 @@ void writeback(string destReg, int value, bool memtoReg, bool memWrite){
     }
     else {
         if(memtoReg){
-            registerfile[destReg] = value;
+            registerfile[destReg] = alu_result;
             cout << destReg + " is modified to 0x" << std::hex << registerfile[destReg] << endl;
         }
         else{
             if(memWrite) {
-                write_dmem(value, registerfile[destReg]);
-                cout << "memory 0x" << std::hex << value << " is modified to 0x" << std::hex << registerfile[destReg] << endl; 
+                write_dmem(alu_result, registerfile[destReg]);
+                cout << "memory 0x" << std::hex << alu_result << " is modified to 0x" << std::hex << registerfile[destReg] << endl; 
             }
             else {
-               registerfile[destReg] = read_dmem(value);
+               registerfile[destReg] = read_dmem(alu_result);
                cout << destReg + " is modified to 0x" << std::hex << registerfile[destReg] << endl; 
             }
         }
